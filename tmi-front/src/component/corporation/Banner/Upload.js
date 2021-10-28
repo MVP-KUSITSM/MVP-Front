ff408f24845eb83e8344f9281dcc178da9dc0bf5
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-10-28 13:48:29 +0900

    [style] 기업-배너 업로드 프레임 구성

--- tmi-front/src/component/corporation/Banner/Upload.js
+++ tmi-front/src/component/corporation/Banner/Upload.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import "../../../assets/corporation/banner/upload.css";
 
 function Upload() {
-
   return (
-    <div>
-        Upload
+    <div class="Banner-Upload">
+        <div class="title">
+            <p>업로드한 배너</p>
+        </div>
+        <div class="banner-add">
+          <div class="banner-add-text">
+            <p>배너추가</p>
+          </div>
+        </div>
     </div>
   );
 }
