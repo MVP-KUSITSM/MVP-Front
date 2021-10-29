41ae7dab8e73e69ee7afeba514868494af4cb950
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-10-29 22:36:00 +0900

    [style] banner/prototype detail, GNB, LNB 1차

--- tmi-front/src/component/corporation/Banner/Upload.js
+++ tmi-front/src/component/corporation/Banner/Upload.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
-import "../../../assets/corporation/banner/upload.css";
+import "../../../assets/css/corporation/banner/upload.css";
+import GNB from "../GNB/GNB";
+import LNB from "../LNB/LNB";
 
 function Upload() {
   return (
+    <>
+    <GNB />
+    <LNB />
     <div class="Banner-Upload">
-        <div class="title">
-            <p>업로드한 배너</p>
-        </div>
-        <div class="banner-add">
-          <div class="banner-add-text">
-            <p>배너추가</p>
-          </div>
-        </div>
+        배너 업로드
     </div>
+    </>
   );
 }
 
