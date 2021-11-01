feccc694c3b414fb83999e03ecaba5808809a63a
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-11-01 23:37:32 +0900

    [style] 기업-배너-결과 1차 구성

--- tmi-front/src/component/corporation/Banner/Result.js
+++ tmi-front/src/component/corporation/Banner/Result.js
@@ -12,20 +12,22 @@ function Result() {
     <div class="Banner-Result">
         <div class="title">
             <p>펀딩결과</p>
+            <div class="line2"></div>
         </div>
         <div class="banner-favor">
-            <p class="banner-favor-text">배너추가</p>
+            <p class="banner-favor-text">배너 선호도</p>
             <img class="banner-img"></img>
             <p class="favor-scale"></p>
+            <div class="line3"></div>
         </div>
         <div class="banner-statistic">
             <p class="banner-statistic-text">참여자 통계</p>
             <div class="basic-chart">
                 <div class="gender-chart"></div>
-                <div class="gender-chart"></div>
+                <div class="age-chart"></div>
             </div>
-            <div class="gender-chart"></div>
-            <div class="gender-chart"></div>
+            <div class="job-chart"></div>
+            <div class="field-chart"></div>
         </div>
     </div>
     </>
