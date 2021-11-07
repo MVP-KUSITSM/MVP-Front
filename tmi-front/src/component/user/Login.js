c75215c9dd9b1c57fe06aa2bf8135bb899a095d0
Eundongdong <51512016+Eundongdong@users.noreply.github.com>
2021-11-07 16:28:58 +0900

    [feat]로그인 및 회원가입 기본 설계

--- tmi-front/src/component/user/Login.js
+++ tmi-front/src/component/user/Login.js
@@ -1,11 +1,39 @@
 import React, { useState } from "react";
+import "../../assets/css/user/login.css";
+import { Button } from 'react-bootstrap';
+import { Link,Route } from "react-router-dom";
+import Bottomnav from "../mvp/bottomnav";
 
 function Login() {
 
   return (
-    <div>
-        Login
-    </div>
+    <>
+      <div className="all_login">
+          <div>로그인</div>
+          <div className="login">
+            <div>아이디 또는 이메일</div>
+            <input></input>
+
+            <div className="login_else">
+              <div>아이디 or 이메일을 잊으셨나요? <Link to ="/main">아이디찾기</Link></div>
+              <div>계정이 아직 없으신가요? <Link to ="/registerg">회원가입</Link></div>
+            </div>
+            <Button>다음</Button>
+          </div>
+      </div>
+    </>
+  );
+}
+
+function Password(){
+
+  return(
+    <>
+      <div className="password">
+        <div>패스워드</div>
+        <input></input>
+      </div>
+    </>
   );
 }
 
--- tmi-front/src/component/user/RegisterG.js
+++ tmi-front/src/component/user/RegisterG.js
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
+import { Button } from 'react-bootstrap';
+import "../../assets/css/user/registerg.css"
 
 
 function RegisterG() {
 
   return (
-    <div>
-        Register-개인
-    </div>
+    <>
+      <div className ="all">
+        <h5>개인 회원가입</h5>
+        <div className="registerbox">
+          <div>이름</div>
+          <input></input>
+          <div>아이디</div>
+          <input></input>
+          <div>비밀번호</div>
+          <input></input>
+        </div>
+        <Button>생성하기</Button>
+      </div>
+    </>
   );
 }
 
