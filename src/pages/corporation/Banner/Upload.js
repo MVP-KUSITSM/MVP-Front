7995ac349ac880e200e4a71932a6e93de7ba0560
dongdong <jkjk4373@gmail.com>
2021-11-20 18:40:01 +0900

    [etc]

--- src/pages/corporation/Banner/Upload.js
+++ src/pages/corporation/Banner/Upload.js
@@ -14,20 +14,6 @@ function Upload() {
     <>
     <GNB />
     <LNB />
-    <div class="Banner-Upload">
-        <p class="title">배너 업로드</p>
-        <p class="description">
-          배너 펀딩에 참여하여 사용자들의 선호도를 알아볼 배너 이미지 파일을 업로드 하세요.
-        <br/>
-        파일은 최대 n개까지 업로드할 수 있어요.</p>
-        <div class="file-select">
-          <input type="file" onChange={handleSelectFile} class="file-select-btn" />파일 선택 버튼
-          <p class="file-select-description">또는 파일을 여기에 드래그 앤 드롭 하세요.</p>
-        </div>
-        <div class="line"></div>
-        <p class="banner-description-text">배너 설명</p>
-        <input class="banner-description" placeholder="배너에 대한 간단한 설명을 입력하세요."></input>
-    </div>
     </>
   );
 }
