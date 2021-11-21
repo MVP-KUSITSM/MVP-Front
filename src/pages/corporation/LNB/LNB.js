4b0c8c902a3b8396fdf08d00580c4322c16e18ca
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-11-21 15:49:09 +0900

    git 수정

--- src/pages/corporation/LNB/LNB.js
+++ src/pages/corporation/LNB/LNB.js
@@ -4,15 +4,15 @@ import "../../../assets/css/corporation/lnb.css"
 function LNB() {
 
   return (
-    <>
     <div class="Left-Menu">
-      <div class="menu1">대시보드</div>
-      <div class="menu2">배너 관리</div>
-      <div class="menu3">프로토타입 관리</div>
-      <div class="menu4">타제품 통계 자료</div>
-      <div class="menu5">전문가 컨설팅</div>
+        <div class="content-menu">
+            <div class="menu1">대시보드</div>
+            <div class="menu2">배너 관리</div>
+            <div class="menu3">프로토타입 관리</div>
+            <div class="menu4">타제품 통계 자료</div>
+            <div class="menu5">전문가 컨설팅</div>
+        </div>
     </div>
-    </>
   );
 }
 
